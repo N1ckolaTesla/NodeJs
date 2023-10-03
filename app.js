162370e911becb2a83496fe2696180a1e51d94bf
nicola <jeremy3773@mail.ru>
2023-10-03 10:49:23 -0400

    mongo db

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 //routes which should handle requests
 const productsRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
+
+mongoose.connect('mongodb://localhost:27017/')
 
 //использует morgan для отображения процессов в терминале
 app.use(morgan('dev'))
