3846656657ebe16ba7ff5ebf69920b84cfe9a793
Nick <jeremy3773@mail.ru>
2023-10-09 14:42:29 +0300

    creating a token and comparing it when we are loginning

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.get('/users', (req, res, next) => {
     User.find() //находит все элементы
@@ -26,7 +27,7 @@ router.get('/users', (req, res, next) => {
 })
 
 router.post('/signup', (req, res, next) => {
-    User.find({email: req.body.email})
+    User.find({email: req.body.email})//проверяем, нет ли уже зарегестрированного пользователя с данным маилом
     .exec()
     .then(user => {
         if (user.length >= 1) {//потому что если пользователь не существует, то user будет равен пустому массиву
@@ -61,6 +62,45 @@ router.post('/signup', (req, res, next) => {
                 }
             })
         }
+    })
+})
+
+router.post('/login', (req, res, next) => {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {//тут мы получим массив с максимум одним пользователем, потому что мы предотвращаем создание новых пользователей с одного маила
+        if (user.length < 1) {
+            return res.status(401).json({//делаем это для того, чтобы мошенники не понимали, существует ли пользователь с таким маилом
+                message: 'Auth failed' 
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if (err) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                })
+            }
+            if (result) {
+                const token = jwt.sign(
+                    {email: user[0].email, userId: user[0]._id}, 
+                    process.env.JWT_KEY,
+                    {expiresIn: '1h'}
+                )
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token
+                })
+            }
+            res.status(401).json({ //если пароль неверный
+                message: 'Auth failed'
+            })
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
